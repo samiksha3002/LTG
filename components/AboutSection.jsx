3b473d751574e9f9be464cebb50d979f33472c92
samiksha3002 <Samiksha.jaiswal.bca@srwc.raisoni.net>
2025-06-19 20:10:01 +0530

    yz

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -16,7 +16,7 @@ export default function AboutSection() {
 
   return (
     <section
-      className="bg-[#dcd8c7] py-16 px-6 md:px-20"
+      className="bg-[#F9F9F9] py-16 px-6 md:px-20"
       aria-labelledby="about-us-heading"
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
